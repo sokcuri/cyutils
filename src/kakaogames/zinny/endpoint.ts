8aca2629361b209d9f95e80d10fe36442df7b31d
sokcuri <sokcuri@gmail.com>
2020-04-12 20:46:46 +0900

    endpoint disable eslint

--- src/kakaogames/zinny/endpoint.ts
+++ src/kakaogames/zinny/endpoint.ts
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/camelcase */
 export enum EndPoint {
   Test = 'https://postman-echo.com/get',
   Naver = 'https://naver.com/ab4ab34b',
-  InfoDeskApp = 'https://infodesk-zinny3.game.kakao.com/v2/app',
-  AccessTokenCreateWithPreviousInfo = 'https://openapi-zinny3.game.kakao.com/service/v3/device/accessToken/createWithPreviousInfo'
+  InfoDesk_App = 'https://infodesk-zinny3.game.kakao.com/v2/app',
+  AccessToken_CreateWithPreviousInfo = 'https://openapi-zinny3.game.kakao.com/service/v3/device/accessToken/createWithPreviousInfo'
 }
 
 export default EndPoint;
